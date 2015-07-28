1f67ff5563ca2e0d85d434e24050a6083323c7dc
wenjiezhang2013 <wenjiezhang2013@gmail.com>
2015-07-27 23:43:33 -0700

    ?

---
+++ src/couresra/week1/assignment/Exercise1.scala
@@ -0,0 +1,25 @@
+package couresra.week1.assignment
+
+/**
+ * Exercise 1: Pascal’s Triangle
+ * https://class.coursera.org/progfun-005/assignment/view?assignment_id=4
+ * @author wenjiezhang
+ */
+class Exercise1 {
+//      1
+//     1 1
+//    1 2 1
+//   1 3 3 1
+//  1 4 6 4 1
+  
+//  Do this exercise by implementing the pascal function in Main.scala, 
+//  which takes a column c and a row r, counting from 0 and returns the number at 
+//  that spot in the triangle. For example, pascal(0,2)=1, pascal(1,2)=2 and pascal(1,3)=3.
+  def pascal(c: Int, r: Int): Int = {
+    var result = 1;
+    
+    
+    result
+  }
+  
+}
